9d6bb46f2515eb24ad6999e1a02b37c7ed2e2825
blue32a <blue32a.jp@gmail.com>
2023-06-02 10:19:09 +0000

    totalAmountのループを分割する

--- src/part1/02-decomposing-the-statement-function/statement.js
+++ src/part1/02-decomposing-the-statement-function/statement.js
@@ -3,8 +3,9 @@ function statement (invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
-    // 注文の内訳を出力
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+  }
+  for (let perf of invoice.performances) {
     totalAmount += amountFor(perf);
   }
 
