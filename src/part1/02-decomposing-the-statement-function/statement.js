28a5a3f89f08d71582d56ccc2312da07b9753ec5
blue32a <blue32a.jp@gmail.com>
2023-06-02 06:45:51 +0000

    volumeCreditsFor関数内の変数名を理解しやすいものに変更

--- src/part1/02-decomposing-the-statement-function/statement.js
+++ src/part1/02-decomposing-the-statement-function/statement.js
@@ -7,11 +7,8 @@ function statement (invoice, plays) {
                           minimumFractionDigits: 2 }).format;
 
   for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
 
-    // ボリューム得点のポイントを加算
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // 喜劇のときは 10 人につき、さらにポイントを加算
-    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
     // 注文の内訳を出力
     result += `  ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
@@ -45,6 +42,13 @@ function statement (invoice, plays) {
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+    return result;
   }
 }
 
