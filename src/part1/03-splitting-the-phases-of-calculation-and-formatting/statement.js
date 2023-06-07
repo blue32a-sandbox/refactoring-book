b7dee10114dd0fd04715868e3c62f22db46d8c79
blue32a <blue32a.jp@gmail.com>
2023-06-07 14:08:47 +0000

    関数volumeCreditsForを関数statement側に移動

--- src/part1/03-splitting-the-phases-of-calculation-and-formatting/statement.js
+++ src/part1/03-splitting-the-phases-of-calculation-and-formatting/statement.js
@@ -8,6 +8,7 @@ function statement (invoice, plays) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
     result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
   function playFor(aPerformance) {
@@ -34,6 +35,12 @@ function statement (invoice, plays) {
     }
     return result;
   }
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
 }
 
 function renderPlainText(data, plays) {
@@ -55,7 +62,7 @@ function renderPlainText(data, plays) {
   function totalVolumeCredits() {
     let result = 0;
     for (let perf of data.performances) {
-      result += volumeCreditsFor(perf);
+      result += perf.volumeCredits;
     }
     return result;
   }
@@ -63,12 +70,6 @@ function renderPlainText(data, plays) {
     return new Intl.NumberFormat("en-US",
                         { style: "currency", currency: "USD",
                           minimumFractionDigits: 2 }).format(aNumber/100);
-  }
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-    return result;
   }
 }
 
