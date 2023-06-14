f7da1bb01967c7c5602a99a267ab392584179a5e
blue32a <blue32a.jp@gmail.com>
2023-06-14 15:21:30 +0000

    ボリューム特典ポイント計算の条件記述を置き換える
    ポリモーフィズムによる条件記述の置き換え

--- src/part1/04-reorganizing-the-calculations-by-type/createStatementData.js
+++ src/part1/04-reorganizing-the-calculations-by-type/createStatementData.js
@@ -40,27 +40,10 @@ class PerformanceCalculator {
     this.play = aPlay;
   }
   get amount() {
-    let result = 0;
-    switch (this.play.type) {
-      case "tragedy":
-        throw "想定外の呼び出し";
-      case "comedy":
-        result = 30000;
-        if (this.performance.audience > 20) {
-          result += 10000 + 500 * (this.performance.audience - 20);
-        }
-        result += 300 * this.performance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${this.play.type}`);
-    }
-    return result;
+    throw new Error("サブクラスの責務");
   }
   get volumeCredits() {
-    let result = 0;
-    result += Math.max(this.performance.audience - 30, 0);
-    if ("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
-    return result;
+    return Math.max(this.performance.audience - 30, 0);
   }
 }
 class TragedyCalculator extends PerformanceCalculator {
@@ -73,4 +56,15 @@ class TragedyCalculator extends PerformanceCalculator {
   }
 }
 class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result;
+  }
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
+  }
 }
