7d3a7cde96625bd860bdb938fea23e9e63c5d333
blue32a <blue32a.jp@gmail.com>
2023-06-19 18:18:01 +0000

    境界値の検査

---
+++ src/ch04/05-probing-the-boundaries/production.test.js
@@ -0,0 +1,64 @@
+import { Province, sampleProvinceData } from "./production";
+
+describe('province', () => {
+  let asia;
+  beforeEach(() => {
+    asia = new Province(sampleProvinceData());
+  });
+  test('shortfall', () => {
+    expect(asia.shortfall).toBe(5);
+  });
+  test('profit', () => {
+    expect(asia.profit).toBe(230);
+  });
+  test('change production', () => {
+    asia.producers[0].production = 20;
+    expect(asia.shortfall).toBe(-6);
+    expect(asia.profit).toBe(292);
+  });
+  test('zero demand', () => {
+    asia.demand = 0;
+    expect(asia.shortfall).toBe(-25);
+    expect(asia.profit).toBe(0);
+  });
+  test('negative demand', () => {
+    asia.demand = -1;
+    expect(asia.shortfall).toBe(-26);
+    expect(asia.profit).toBe(-10);
+  });
+  test('empty string demand', () => {
+    asia.demand = "";
+    expect(asia.shortfall).toBeNaN();
+    expect(asia.profit).toBeNaN();
+  });
+});
+describe('no producers', () => {
+  let noProducers;
+  beforeEach(() => {
+    const data = {
+      name: "No producers",
+      producers: [],
+      demand: 30,
+      price: 20
+    };
+    noProducers = new Province(data);
+  });
+  test('shortfall', () => {
+    expect(noProducers.shortfall).toBe(30);
+  });
+  test('profit', () => {
+    expect(noProducers.profit).toBe(0);
+  });
+});
+describe('string for producers', () => {
+  test('', () => {
+    const data = {
+      name: "String producers",
+      producers: "",
+      demand: 30,
+      price: 20
+    }
+    const prov = new Province(data);
+    expect(prov.shortfall).toBe(0);
+  });
+});
