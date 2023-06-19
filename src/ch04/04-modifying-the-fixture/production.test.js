2be955b727872fa985a9ef867a04a6915c0d7ae0
blue32a <blue32a.jp@gmail.com>
2023-06-19 18:03:40 +0000

    フィクスチャの変更

---
+++ src/ch04/04-modifying-the-fixture/production.test.js
@@ -0,0 +1,19 @@
+import { Province, sampleProvinceData } from "./production";
+
+describe('province', () => {
+  let asia;
+  beforeEach(() => {
+    asia = new Province(sampleProvinceData());
+  });
+  test('shortfall', () => {
+    expect(asia.shortfall).toBe(5);
+  });
+  test('profit', () => {
+    expect(asia.profit).toBe(230);
+  });
+  test('change production', () => {
+    asia.producers[0].production = 20;
+    expect(asia.shortfall).toBe(-6);
+    expect(asia.profit).toBe(292);
+  });
+});
