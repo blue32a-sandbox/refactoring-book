1eae7ab1116d5d3ceb055b7021adfbcb872a9ac2
blue32a <blue32a.jp@gmail.com>
2023-07-12 09:12:44 +0000

    最も単純なケースー関数のインライン化

---
+++ src/ch06/02-inline-function/example01.js
@@ -0,0 +1,11 @@
+/**
+ * 最も単純なケース
+ */
+
+function rating(aDriver) {
+  return moreThanFiveLateDeliveries(aDriver) ? 2 : 1;
+}
+
+function moreThanFiveLateDeliveries(aDriver) {
+  return aDriver.numberOfLateDeliveries > 5;
+}
