a11e616f77748cf348aa7eeca9bf8358f46aac6a
blue32a <blue32a.jp@gmail.com>
2023-07-12 08:53:05 +0000

    抽出しようとするコードをコピーして関数にするー関数の抽出

--- src/ch06/01-extract-function/printOwing.js
+++ src/ch06/01-extract-function/printOwing.js
@@ -10,6 +10,13 @@ function printOwing(invoice) {
   recordDueDate(invoice);
   printDetails(invoice, outstanding);
 }
+function calculateOutstanding(invoice) {
+  let outstanding = 0;
+  for (const o of invoice.orders) {
+    outstanding += o.amount;
+  }
+  return outstanding;
+}
 function recordDueDate(invoice) {
   const today = Clock.today;
   invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
