dc2f89e70c6d867a4d2368aad6633332757f7282
blue32a <blue32a.jp@gmail.com>
2023-07-12 08:52:15 +0000

    変数の宣言を参照されている行の近くに移動ー関数の抽出

--- src/ch06/01-extract-function/printOwing.js
+++ src/ch06/01-extract-function/printOwing.js
@@ -1,9 +1,8 @@
 function printOwing(invoice) {
-  let outstanding = 0;
-
   printBanner();
 
   // 未払い金の計算（calculate outstanding）
+  let outstanding = 0;
   for (const o of invoice.orders) {
     outstanding += o.amount;
   }
