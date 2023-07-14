4ad56f7572bf3186cf1e09b90b58f557281f16cb
blue32a <blue32a.jp@gmail.com>
2023-07-14 14:07:16 +0000

    値のカプセル化の例ー変数のカプセル化

--- src/ch06/06-encapsulate-variable/app.js
+++ src/ch06/06-encapsulate-variable/app.js
@@ -1,9 +1,8 @@
+import assert from 'assert';
 import { defaultOwner, setDefaultOwner } from "./defaultOwner.js";
 
-const spaceship = {};
-spaceship.owner = defaultOwner();
-
-setDefaultOwner({firstName: "Rebecca", lastName: "Parsons"});
-
-console.log(spaceship.owner);
-console.log(defaultOwner());
+const owner1 = defaultOwner();
+assert.equal("Fowler", owner1.lastName, "when set");
+const owner2 = defaultOwner();
+owner2.lastName = "Parsons";
+assert.notEqual("Parsons", owner1.lastName, "after change owner2");
