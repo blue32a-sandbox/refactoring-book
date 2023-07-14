aed52b997653cfedaebee1b2d7bf917fbac4a1b9
blue32a <blue32a.jp@gmail.com>
2023-07-14 14:11:50 +0000

    `レコードのカプセル化`を施す方法ー変数のカプセル化

--- src/ch06/06-encapsulate-variable/defaultOwner.js
+++ src/ch06/06-encapsulate-variable/defaultOwner.js
@@ -1,4 +1,15 @@
 let defaultOwnerData = {firstName: "Martin", lastName: "Fowler"};
 
-export function defaultOwner() {return Object.assign({}, defaultOwnerData);}
+export function defaultOwner() {return new Person(defaultOwnerData);}
 export function setDefaultOwner(arg) {defaultOwnerData = arg;}
+
+class Person {
+  constructor(data) {
+    this._lastName = data.lastName;
+    this._firstName = data.firstName;
+  }
+  get lastName() {return this._lastName;}
+  set lastName(lastName) {this._lastName = lastName;}
+  get firstName() {return this._firstName};
+  set firstName(firstName) {this._firstName = firstName;}
+}
