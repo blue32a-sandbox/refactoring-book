c466f497321f4a1df7c051685a55c1ae00c0200a
blue32a <blue32a.jp@gmail.com>
2023-07-21 14:06:29 +0000

    データをまとめるためのクラスを定義ーパラメータオブジェクトの導入

--- src/ch06/08-introduce-parameter-object/temperature.js
+++ src/ch06/08-introduce-parameter-object/temperature.js
@@ -1,3 +1,11 @@
+class NumberRange {
+  constructor(min, max) {
+    this._data = {min: min, max: max};
+  }
+  get min() {return this._data.min;}
+  get max() {return this._data.max;}
+}
+
 const station = {
   name: "ZB1",
   readings: [
