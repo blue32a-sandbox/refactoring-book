378179a96934ae62f647f0773b9a793c8ab158ef
blue32a <blue32a.jp@gmail.com>
2023-07-31 15:41:35 +0000

    課税対象額の計算に`関数の抽出`を施すー関数群のクラスへの集約

--- src/ch06/09-combine-functions-into-class/client2.js
+++ src/ch06/09-combine-functions-into-class/client2.js
@@ -3,5 +3,9 @@ import { Reading } from './reading.js';
 
 const rawReading = acquireReading();
 const aReading = new Reading(rawReading);
-const taxbleCharge = Math.max(0, aReading.baseCharge - taxThreshold(aReading.year));
+const taxbleCharge = taxableChangeFn(aReading);
 console.log(taxbleCharge);
+
+function taxableChangeFn(aReading) {
+  return Math.max(0, aReading.baseCharge - taxThreshold(aReading.year));
+}
