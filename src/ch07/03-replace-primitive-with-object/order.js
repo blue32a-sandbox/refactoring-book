b421ce1bb575949cb28ac80f8fd58f43b9c88987
blue32a <blue32a.jp@gmail.com>
2023-08-25 14:47:33 +0000

    Priorityオブジェクトを直接返すーオブジェクトによるプリミティブの置き換え

--- src/ch07/03-replace-primitive-with-object/order.js
+++ src/ch07/03-replace-primitive-with-object/order.js
@@ -2,6 +2,7 @@ export class Order{
   constructor(data) {
     this.priority = data.priority;
   }
+  get priority() {return this._priority;}
   get priorityString() {return this._priority.toString();}
   set priority(aString) {this._priority = new Priority(aString);}
 }
