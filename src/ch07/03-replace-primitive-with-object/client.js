6181f07c81dce743091c5c61e584a403fd323375
blue32a <blue32a.jp@gmail.com>
2023-08-25 14:44:52 +0000

    `関数名の変更`を施すーオブジェクトによるプリミティブの置き換え

--- src/ch07/03-replace-primitive-with-object/client.js
+++ src/ch07/03-replace-primitive-with-object/client.js
@@ -6,7 +6,7 @@ orders.push(new Order({priority: "normal"}));
 orders.push(new Order({priority: "high"}));
 orders.push(new Order({priority: "rush"}));
 
-const highPriorityCount = orders.filter(o => "high" === o.priority
-                                          || "rush" === o.priority)
+const highPriorityCount = orders.filter(o => "high" === o.priorityString
+                                          || "rush" === o.priorityString)
                                 .length;
 console.log(highPriorityCount);
--- src/ch07/03-replace-primitive-with-object/order.js
+++ src/ch07/03-replace-primitive-with-object/order.js
@@ -2,7 +2,7 @@ export class Order{
   constructor(data) {
     this.priority = data.priority;
   }
-  get priority() {return this._priority.toString();}
+  get priorityString() {return this._priority.toString();}
   set priority(aString) {this._priority = new Priority(aString);}
 }
 
