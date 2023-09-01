6fff15f892cdf8ffbe17eb8d1449a1e19b8b0318
blue32a <blue32a.jp@gmail.com>
2023-09-01 13:10:04 +0000

    `変数のインライン化`を施すー問い合わせによる一時変数の置き換え

--- src/ch07/04-replace-temp-with-query/order.js
+++ src/ch07/04-replace-temp-with-query/order.js
@@ -4,10 +4,9 @@ export class Order {
     this._item = item;
   }
   get price() {
-    const basePrice = this.basePrice;
     var discountFactor = 0.98;
-    if (basePrice > 1000) discountFactor -= 0.03;
-    return basePrice * discountFactor;
+    if (this.basePrice > 1000) discountFactor -= 0.03;
+    return this.basePrice * discountFactor;
   }
   get basePrice() {
     return this._quantity * this._item.price;
