dc37210e1865d6b87cdd20a0e47495e625f62630
blue32a <blue32a.jp@gmail.com>
2023-09-01 13:04:51 +0000

    代入の右辺をgetterとして抽出ー問い合わせによる一時変数の置き換え

--- src/ch07/04-replace-temp-with-query/order.js
+++ src/ch07/04-replace-temp-with-query/order.js
@@ -4,9 +4,12 @@ export class Order {
     this._item = item;
   }
   get price() {
-    const basePrice = this._quantity * this._item.price;
+    const basePrice = this.basePrice;
     var discountFactor = 0.98;
     if (basePrice > 1000) discountFactor -= 0.03;
     return basePrice * discountFactor;
+  }
+  get basePrice() {
+    return this._quantity * this._item.price;
   }
 }
