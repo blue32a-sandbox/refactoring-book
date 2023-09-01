515ea8ac3f28a120d104f051ef248872bb87b3f5
blue32a <blue32a.jp@gmail.com>
2023-09-01 13:30:18 +0000

    TelephoneNumberのインスタンスを作るークラスの抽出

--- src/ch07/05-extract-class/person.js
+++ src/ch07/05-extract-class/person.js
@@ -1,4 +1,7 @@
 class Person {
+  constructor() {
+    this._telephoneNumber = new TelephoneNumber();
+  }
   get name() {return this._name;}
   set name(arg) {this._name = arg;}
   get telephoneNumber() {return `(${this.officeAreaCode}) ${this.officeNumber}`;}
