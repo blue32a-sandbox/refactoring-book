5f279c8848e121c4fff317f77b20c51e74d5400b
blue32a <blue32a.jp@gmail.com>
2023-09-13 13:23:32 +0000

    ある人の上司を調べる例ー委譲の隠蔽

---
+++ src/ch07/06-hide-delegate/company.js
@@ -0,0 +1,15 @@
+export class Person {
+    constructor(name) {
+        this._name = name;
+    }
+    get name() { return this._name; }
+    get department() { return this._department; }
+    set department(arg) { this._department = arg; }
+}
+
+export class Department {
+    get chargeCode() {return this._chargeCode;}
+    set chargeCode(arg) {this._chargeCode = arg;}
+    get manager() {return this._manager;}
+    set manager(arg) {this._manager = arg;}
+}
