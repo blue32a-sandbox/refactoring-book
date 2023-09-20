6610b9f0c9600d75040628377b15f00dec7596ee
blue32a <blue32a.jp@gmail.com>
2023-09-20 13:59:52 +0000

    `関数の抽出`を行うーステートメントの関数内への移動

--- src/ch08/03-move-statements-info-function/render.js
+++ src/ch08/03-move-statements-info-function/render.js
@@ -2,17 +2,22 @@ function renderPerson(outStream, person) {
     const result = [];
     result.push(`<p>${person.name}</p>`);
     result.push(renderPhoto(person.photo));
-    result.push(`<p>title: ${person.photo.title}</p>`);
-    result.push(emitPhotoData(person.photo));
+    result.push(zznew(person.photo));
     return result.join("\n");
 }
 
 function photoDiv(p) {
     return [
         "<div>",
+        zznew(p),
+        "</div>",
+    ].join("\n");
+}
+
+function zznew(p) {
+    return [
         `<p>title: ${p.title}</p>`,
         emitPhotoData(p),
-        "</div>",
     ].join("\n");
 }
 
