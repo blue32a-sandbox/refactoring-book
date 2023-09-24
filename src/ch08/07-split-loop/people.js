a559f99f7264f21888118c6289466122dea41b82
blue32a <blue32a.jp@gmail.com>
2023-09-24 12:58:24 +0000

    `アルゴリズムの置き換え`ーループの分離

--- src/ch08/07-split-loop/people.js
+++ src/ch08/07-split-loop/people.js
@@ -5,9 +5,5 @@ function totalSalary() {
 }
 
 function youngestAge() {
-  let youngest = people[0] ? people[0].age : Infinity;
-  for (const p of people) {
-    if (p.age < youngest) youngest = p.age;
-  }
-  return youngest;
+  return Math.min(...people.map(p => p.age));
 }
