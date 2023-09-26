1d037b52c5d47f48e1b9032aa28550c4b10240ca
blue32a <blue32a.jp@gmail.com>
2023-09-26 13:40:18 +0000

    `関数名の変更`を行いアクセサを変更するーフィールド名の変更

--- src/ch09/02-rename-field/organization.js
+++ src/ch09/02-rename-field/organization.js
@@ -3,8 +3,8 @@ class Organization {
     this._title = data.title;
     this._country = data.country;
   }
-  get name() {return this._title;}
-  set name(aString) {this._title = aString;}
+  get title() {return this._title;}
+  set title(aString) {this._title = aString;}
   get country() {return this._country;}
   set country(aCountryCode) {this._country = aCountryCode;}
 }
