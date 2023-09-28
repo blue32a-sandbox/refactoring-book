add9c4351bc29c7750339ba3318523c520f95303
blue32a <blue32a.jp@gmail.com>
2023-09-28 14:12:21 +0000

    注文のコンストラクタで適切な顧客オブジェクトを取得するー値から参照への変更

--- src/ch09/05-change-value-to-reference/order.js
+++ src/ch09/05-change-value-to-reference/order.js
@@ -1,7 +1,9 @@
+import { registerCustomer } from "./customer.js";
+
 class Order {
   constructor(data) {
     this._number = data.number;
-    this._customer = new Customer(data.customer);
+    this._customer = registerCustomer(data.customer)
     // 他のデータをロードする
   }
   get customer() {return this._customer;}
