02d2d588d9e9afcf85b7e3c31e120577376b6378
blue32a <blue32a.jp@gmail.com>
2023-09-29 14:31:31 +0000

    then節を関数に抽出ー条件記述の分解

--- src/ch10/01-decompose-conditional/price.js
+++ src/ch10/01-decompose-conditional/price.js
@@ -1,8 +1,11 @@
 if (summer())
-  charge = quantity * plan.summerRate;
+  charge = summerCharge();
 else
   charge = quantity * plan.regularRate + plan.regularServiceCharge;
 
 function summer() {
   return !aData.isBefore(plan.summerStart) && !aData.isAfter(plan.summerEnd);
+}
+function summerCharge() {
+  return quantity * plan.summerRate;
 }
