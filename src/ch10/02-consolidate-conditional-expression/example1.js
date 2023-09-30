d854c1267d756bbdb5a6b6e2fdb79895ef19db74
blue32a <blue32a.jp@gmail.com>
2023-09-30 14:39:39 +0000

    or演算子で条件判定を統合するー条件記述の統合

--- src/ch10/02-consolidate-conditional-expression/example1.js
+++ src/ch10/02-consolidate-conditional-expression/example1.js
@@ -1,6 +1,6 @@
 function disabilityAmount(anEmployee) {
-  if (anEmployee.seniority < 2) return 0;
-  if (anEmployee.monthsDisabled > 12) return 0;
-  if (anEmployee.isPartTime) return 0;
+  if ((anEmployee.seniority < 2)
+      || (anEmployee.monthsDisabled > 12)
+      || (anEmployee.isPartTime)) return 0;
   // 障害手当を計算する
 }
