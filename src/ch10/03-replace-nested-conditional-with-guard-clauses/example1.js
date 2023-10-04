c8056d8230f09e38c7e46a3490bcff92c43c49ad
blue32a <blue32a.jp@gmail.com>
2023-10-04 13:27:52 +0000

    従業員への支払額を計算するコードの例ーガード節による入れ子の条件記述の置き換え

---
+++ src/ch10/03-replace-nested-conditional-with-guard-clauses/example1.js
@@ -0,0 +1,20 @@
+function payAmount(employee) {
+  let result;
+  if (employee.isSeparated) {
+    result = {amount: 0, reasonCode: "SEP"};
+  }
+  else {
+    if (employee.isRetired) {
+      result = {amount: 0, reasonCode: "RET"};
+    }
+    else {
+      // 金額を計算するロジック
+      lorem.ipsum(dolor.sitAmet);
+      consectetur(adipiscing).elit();
+      sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+      ut.enim.ad(minim.veniam);
+      result = someFinalComputation();
+    }
+  }
+  return result;
+}
