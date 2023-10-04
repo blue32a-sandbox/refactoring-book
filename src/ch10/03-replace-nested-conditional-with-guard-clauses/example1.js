9257c2adbc8d04ff568001d5c92fb1d550d634e7
blue32a <blue32a.jp@gmail.com>
2023-10-04 13:30:57 +0000

    退職者の判定にガード節を使うーガード節による入れ子の条件記述の置き換え

--- src/ch10/03-replace-nested-conditional-with-guard-clauses/example1.js
+++ src/ch10/03-replace-nested-conditional-with-guard-clauses/example1.js
@@ -1,16 +1,12 @@
 function payAmount(employee) {
   let result;
   if (employee.isSeparated) return  {amount: 0, reasonCode: "SEP"};
-  if (employee.isRetired) {
-    result = {amount: 0, reasonCode: "RET"};
-  }
-  else {
-    // 金額を計算するロジック
-    lorem.ipsum(dolor.sitAmet);
-    consectetur(adipiscing).elit();
-    sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-    ut.enim.ad(minim.veniam);
-    result = someFinalComputation();
-  }
+  if (employee.isRetired) return {amount: 0, reasonCode: "RET"};
+  // 金額を計算するロジック
+  lorem.ipsum(dolor.sitAmet);
+  consectetur(adipiscing).elit();
+  sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+  ut.enim.ad(minim.veniam);
+  result = someFinalComputation();
   return result;
 }
