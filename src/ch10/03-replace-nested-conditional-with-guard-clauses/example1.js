c6bc5baa9464817639d7b2c50d1216589273f4f0
blue32a <blue32a.jp@gmail.com>
2023-10-04 13:31:28 +0000

    result変数を削除するーガード節による入れ子の条件記述の置き換え

--- src/ch10/03-replace-nested-conditional-with-guard-clauses/example1.js
+++ src/ch10/03-replace-nested-conditional-with-guard-clauses/example1.js
@@ -1,5 +1,4 @@
 function payAmount(employee) {
-  let result;
   if (employee.isSeparated) return  {amount: 0, reasonCode: "SEP"};
   if (employee.isRetired) return {amount: 0, reasonCode: "RET"};
   // 金額を計算するロジック
@@ -7,6 +6,5 @@ function payAmount(employee) {
   consectetur(adipiscing).elit();
   sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
   ut.enim.ad(minim.veniam);
-  result = someFinalComputation();
-  return result;
+  return someFinalComputation();
 }
