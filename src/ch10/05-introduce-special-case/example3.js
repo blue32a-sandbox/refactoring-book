d743015c71c3a0111b7e27fc61da824015a06ae8
blue32a <blue32a.jp@gmail.com>
2023-10-08 12:57:58 +0000

    未知の顧客の判定に対して`関数の抽出`を行うー特殊ケースの導入

--- src/ch10/05-introduce-special-case/example3.js
+++ src/ch10/05-introduce-special-case/example3.js
@@ -4,21 +4,25 @@ function enrichSite(inputSite) {
   return _.cloneDeep(inputSite);
 }
 
+function isUnknown(arg) {
+  return arg === "unknown";
+}
+
 // Client 1...
 const rawSite = acquireSiteData();
 const site = enrichSite(rawSite);
 const aCustomer = site.customer;
 // ... 大量のコードが入る ...
 let customerName;
-if (aCustomer === "unknown") customerName = "occupant";
+if (isUnknown(aCustomer)) customerName = "occupant";
 else customerName = aCustomer.name;
 
 // Client 2...
-const plan = (aCustomer === "unknown") ?
+const plan = (isUnknown(aCustomer)) ?
       registry.billingPlans.basic
       : aCustomer.billingPlan;
 
 // Client 3...
-const weeksDelinquent = (aCustomer === "unknown") ?
+const weeksDelinquent = (isUnknown(aCustomer)) ?
       0
       : aCustomer.paymentHistory.weeksDelinquentInLastYear;
