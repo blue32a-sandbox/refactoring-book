4e34b401836d79053d8a0ccfec3c8b4d4a6fe27e
blue32a <blue32a.jp@gmail.com>
2023-10-08 11:59:44 +0000

    `ステートメントの呼び出し側への移動`を適用するーポリモーフィズムによる条件記述の置き換え

--- src/ch10/04-replace-conditional-with-polymorphism/example2.js
+++ src/ch10/04-replace-conditional-with-polymorphism/example2.js
@@ -16,7 +16,6 @@ class ExperiencedChinaRating extends Rating {
   get voyageAndHistoryLengthFactor() {
     let result = 0;
     result += 3;
-    result += this.historyLengthFactor;
     if (this.voyage.length > 12) result += 1;
     if (this.voyage.length > 18) result -= 1;
     return result;
@@ -55,12 +54,12 @@ class Rating {
     let result = 2;
     if (this.voyage.zone === "china") result += 1;
     if (this.voyage.zone === "east-indies") result += 1;
+    result += this.historyLengthFactor;
     result += this.voyageAndHistoryLengthFactor;
     return result;
   }
   get voyageAndHistoryLengthFactor() {
     let result = 0;
-    result += this.historyLengthFactor;
     if (this.voyage.length > 14) result -= 1;
     return result;
   }
