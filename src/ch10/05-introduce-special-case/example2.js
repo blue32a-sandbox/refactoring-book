ce36dbbc085c139b7c9839a869d59a511258389c
blue32a <blue32a.jp@gmail.com>
2023-10-08 12:47:06 +0000

    特殊ケースの判定に対して`関数の抽出`を行うー特殊ケースの導入

--- src/ch10/05-introduce-special-case/example2.js
+++ src/ch10/05-introduce-special-case/example2.js
@@ -16,19 +16,23 @@ function createUnknownCustomer() {
   };
 }
 
+function isUnknown(arg) {
+  return (arg === "unknown");
+}
+
 // Client 1...
 const aCustomer = site.customer;
 // ... 大量のコードが入る ...
 let customerName;
-if (aCustomer === "unknown") customerName = "occupant";
+if (isUnknown(aCustomer)) customerName = "occupant";
 else customerName = aCustomer.name;
 
 // Client 2...
-const plan = (aCustomer === "unknown") ?
+const plan = isUnknown(aCustomer) ?
       registry.billingPlans.basic
       : aCustomer.billingPlan;
 
 // Client 3...
-const weeksDelinquent = (aCustomer === "unknown") ?
+const weeksDelinquent = isUnknown(aCustomer) ?
       0
       : aCustomer.paymentHistory.weeksDelinquentInLastYear;
