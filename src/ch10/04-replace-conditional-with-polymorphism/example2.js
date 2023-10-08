f7d55d2ba9fcaa50cfe585a9f753315d230e728b
blue32a <blue32a.jp@gmail.com>
2023-10-08 11:58:39 +0000

    `関数の抽出`を行うーポリモーフィズムによる条件記述の置き換え
    名前にAndを含む関数を、別々の処理に分離するため

--- src/ch10/04-replace-conditional-with-polymorphism/example2.js
+++ src/ch10/04-replace-conditional-with-polymorphism/example2.js
@@ -16,10 +16,13 @@ class ExperiencedChinaRating extends Rating {
   get voyageAndHistoryLengthFactor() {
     let result = 0;
     result += 3;
-    if (this.history.length > 10) result += 1;
+    result += this.historyLengthFactor;
     if (this.voyage.length > 12) result += 1;
     if (this.voyage.length > 18) result -= 1;
     return result;
+  }
+  get historyLengthFactor() {
+    return (this.history.length > 10) ? 1 : 0;
   }
 }
 
@@ -57,9 +60,12 @@ class Rating {
   }
   get voyageAndHistoryLengthFactor() {
     let result = 0;
-    if (this.history.length > 8) result += 1;
+    result += this.historyLengthFactor;
     if (this.voyage.length > 14) result -= 1;
     return result;
+  }
+  get historyLengthFactor() {
+    return (this.history.length > 8) ? 1 : 0;
   }
   get hasChinaHistory() {
     return this.history.some(v => "china" === v.zone)
