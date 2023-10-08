212983325c90b6e7566f7096b1e14aa187eb3d8a
blue32a <blue32a.jp@gmail.com>
2023-10-08 12:59:49 +0000

    isUnknownプロパティをCustomerに付与するー特殊ケースの導入

--- src/ch10/05-introduce-special-case/example3.js
+++ src/ch10/05-introduce-special-case/example3.js
@@ -1,7 +1,14 @@
 import _ from "lodash";
 
 function enrichSite(inputSite) {
-  return _.cloneDeep(inputSite);
+  const result = _.cloneDeep(inputSite);
+  const unknownCustomer = {
+    isUnknown: true,
+  }
+
+  if (isUnknown(result.customer)) result.customer = unknownCustomer;
+  else result.customer.isUnknown = false;
+  return result;
 }
 
 function isUnknown(arg) {
