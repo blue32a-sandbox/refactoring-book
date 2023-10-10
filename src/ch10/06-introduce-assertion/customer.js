922e081f4d47abcc6416e09882abd15e5f08d428
blue32a <blue32a.jp@gmail.com>
2023-10-10 22:06:02 +0900

    setterにアサーションを配置するーアサーションの導入
    値がどうやってフィールドに入ったのかが一番の謎になるため。

--- src/ch10/06-introduce-assertion/customer.js
+++ src/ch10/06-introduce-assertion/customer.js
@@ -1,11 +1,13 @@
 import assert from 'assert';
 
 class Customer {
+  set discountRate(aNumber) {
+    assert(null === aNumber || aNumber >= 0);
+    this._discountRate = aNumber;
+  }
   applyDiscount(aNumber) {
-    if (!this.discountRate) return aNumber;
-    else {
-      assert(this.discountRate >= 0);
-      return aNumber - (this.discountRate * aNumber);
-    }
+    return (this.discountRate)
+      ? aNumber - (this.discountRate * aNumber)
+      : aNumber;
   }
 }
