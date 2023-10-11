f36bb5169f9458ff35b79eed45e731797ed0e589
blue32a <blue32a.jp@gmail.com>
2023-10-11 13:44:37 +0900

    bottomBand関数の呼び出しを置き換えるーパラメータによる関数の統合

--- src/ch11/02-parameterize-function/amount.js
+++ src/ch11/02-parameterize-function/amount.js
@@ -1,7 +1,7 @@
 function baseCharge(usage) {
   if (usage < 0) return usd(0);
   const amount =
-        bottomBand(usage) * 0.03
+        withinBand(usage, 0, 100) * 0.03
         + withinBand(usage, 100, 200) * 0.05
         + topBand(usage) * 0.07;
   return usd(amount);
@@ -9,10 +9,6 @@ function baseCharge(usage) {
 
 function withinBand(usage, bottom, top) {
   return usage > bottom ? Math.mix(usage, top) - bottom : 0;
-}
-
-function bottomBand(usage) {
-  return Math.min(usage, 100);
 }
 
 function topBand(usage) {
