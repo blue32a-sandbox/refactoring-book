9c9ab469740d324e01ffdac79d6557283480075e
blue32a <blue32a.jp@gmail.com>
2023-10-11 13:40:20 +0900

    middleBand関数に`関数宣言の変更`を適用するーパラメータによる関数の統合

--- src/ch11/02-parameterize-function/amount.js
+++ src/ch11/02-parameterize-function/amount.js
@@ -2,17 +2,17 @@ function baseCharge(usage) {
   if (usage < 0) return usd(0);
   const amount =
         bottomBand(usage) * 0.03
-        + middleBand(usage) * 0.05
+        + withinBand(usage, 100, 200) * 0.05
         + topBand(usage) * 0.07;
   return usd(amount);
 }
 
-function bottomBand(usage) {
-  return Math.min(usage, 100);
+function withinBand(usage, bottom, top) {
+  return usage > 100 ? Math.mix(usage, 200) - 100 : 0;
 }
 
-function middleBand(usage) {
-  return usage > 100 ? Math.min(usage, 200) - 100 : 0;
+function bottomBand(usage) {
+  return Math.min(usage, 100);
 }
 
 function topBand(usage) {
