6e44eedd09ac61db5243d4cd741b19d84eef84ef
blue32a <blue32a.jp@gmail.com>
2023-10-12 07:53:51 +0000

    仮の関数名を変更するーオブジェクトそのものの受け渡し

--- src/ch11/04-preserve-whole-object/example1.js
+++ src/ch11/04-preserve-whole-object/example1.js
@@ -1,10 +1,10 @@
 class HeatingPlan {
-  xxNEWwithinRange(aNumberRange) {
+  withinRange(aNumberRange) {
     return (aNumberRange.low >= this._tempertureRange.low) &&
       (aNumberRange.high <= this._tempertureRange.high);
   }
 }
 
 // caller ...
-if (!aPlan.xxNEWwithinRange(aRoom.daysTempRange))
+if (!aPlan.withinRange(aRoom.daysTempRange))
   alerts.push("室温が設定値を超えました");
