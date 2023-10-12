dd92aaec0824783c948b2e491eeb198490955459
blue32a <blue32a.jp@gmail.com>
2023-10-12 08:06:12 +0000

    `関数の抽出`を適用するーオブジェクトそのものの受け渡し

--- src/ch11/04-preserve-whole-object/example2.js
+++ src/ch11/04-preserve-whole-object/example2.js
@@ -6,8 +6,14 @@ class HeatingPlan {
 
 // caller ...
 const tempRange = aRoom.daysTempRange;
-const low = tempRange.low;
-const high = tempRange.high;
-const isWithinRange = aPlan.withinRange(low, high);
+const isWithinRange = xxNEWwithinRange(aPlan, tempRange);
 if (!isWithinRange)
   alerts.push("室温が設定値を超えました");
+
+// toplevel ...
+function xxNEWwithinRange(aPlan, tempRange) {
+  const low = tempRange.low;
+  const high = tempRange.high;
+  const isWithinRange = aPlan.withinRange(low, high);
+  return isWithinRange;
+}
