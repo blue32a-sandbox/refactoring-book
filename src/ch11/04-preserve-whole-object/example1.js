b08e754f2683d3c80af2957b6cf3be712090f511
blue32a <blue32a.jp@gmail.com>
2023-10-12 07:47:22 +0000

    部屋の温度管理システムの例ーオブジェクトそのものの受け渡し

---
+++ src/ch11/04-preserve-whole-object/example1.js
@@ -0,0 +1,11 @@
+class HeatingPlan {
+  withinRange(bottom, top) {
+    return (bottom >= this._tempertureRange.low) && (top <= this._tempertureRange.high);
+  }
+}
+
+// caller ...
+const low = aRoom.daysTempRange.low;
+const high = aRoom.daysTempRange.high;
+if (!aPlan.withinRange(low, high))
+  alerts.push("室温が設定値を超えました");
