29cc4bf23b262a3a3e80fae35eb8798c8077b13b
blue32a <blue32a.jp@gmail.com>
2023-10-12 07:51:05 +0000

    `デッドコードの削除`を行うーオブジェクトそのものの受け渡し

--- src/ch11/04-preserve-whole-object/example1.js
+++ src/ch11/04-preserve-whole-object/example1.js
@@ -8,7 +8,5 @@ class HeatingPlan {
 }
 
 // caller ...
-const low = aRoom.daysTempRange.low;
-const high = aRoom.daysTempRange.high;
 if (!aPlan.xxNEWwithinRange(aRoom.daysTempRange))
   alerts.push("室温が設定値を超えました");
