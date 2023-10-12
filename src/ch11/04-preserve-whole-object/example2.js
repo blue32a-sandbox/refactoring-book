a2f45c49d287f07b2f3b289e4c6b67def89f6f8e
blue32a <blue32a.jp@gmail.com>
2023-10-12 08:09:11 +0000

    `関数の移動`を適用するーオブジェクトそのものの受け渡し

--- src/ch11/04-preserve-whole-object/example2.js
+++ src/ch11/04-preserve-whole-object/example2.js
@@ -2,18 +2,16 @@ class HeatingPlan {
   withinRange(bottom, top) {
     return (bottom >= this._tempertureRange.low) && (top <= this._tempertureRange.high);
   }
+  xxNEWwithinRange(tempRange) {
+    const low = tempRange.low;
+    const high = tempRange.high;
+    const isWithinRange = this.withinRange(low, high);
+    return isWithinRange;
+  }
 }
 
 // caller ...
 const tempRange = aRoom.daysTempRange;
-const isWithinRange = xxNEWwithinRange(aPlan, tempRange);
+const isWithinRange = aPlan.xxNEWwithinRange(tempRange);
 if (!isWithinRange)
   alerts.push("室温が設定値を超えました");
-
-// toplevel ...
-function xxNEWwithinRange(aPlan, tempRange) {
-  const low = tempRange.low;
-  const high = tempRange.high;
-  const isWithinRange = aPlan.withinRange(low, high);
-  return isWithinRange;
-}
