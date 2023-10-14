bfc19cdea1d0bcbefb44af8c9c4e3c1325fc4691
blue32a <blue32a.jp@gmail.com>
2023-10-14 07:05:45 +0000

    `関数のインライン化`を適用するーsetterの削除

--- src/ch11/07-remove-setting-method/person.js
+++ src/ch11/07-remove-setting-method/person.js
@@ -1,11 +1,10 @@
 class Person {
   constructor(id) {
-    this.id = id;
+    this._id = id;
   }
   get name()    {return this._name;}
   set name(arg) {this._name = arg;}
   get id()    {return this._id;}
-  set id(arg) {this._id = arg;}
 }
 
 // caller ...
