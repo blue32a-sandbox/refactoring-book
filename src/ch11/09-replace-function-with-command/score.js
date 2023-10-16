41c78f33fc8795496a8bd46bc04280507d51bbe3
blue32a <blue32a.jp@gmail.com>
2023-10-16 05:10:50 +0000

    空のクラスの中へ`関数の移動`を行うーコマンドによる関数の置き換え

--- src/ch11/09-replace-function-with-command/score.js
+++ src/ch11/09-replace-function-with-command/score.js
@@ -1,18 +1,24 @@
 function score(candidate, medicalExam, scoringGuide) {
-  let result = 0;
-  let healthLevel = 0;
-  let highMedicalRiskFlag = false;
+  return new Scorer().execute(candidate, medicalExam, scoringGuide);
+}
 
-  if (medicalExam.isSmoker) {
-    healthLevel += 10;
-    highMedicalRiskFlag = true;
+class Scorer {
+  execute(candidate, medicalExam, scoringGuide) {
+    let result = 0;
+    let healthLevel = 0;
+    let highMedicalRiskFlag = false;
+
+    if (medicalExam.isSmoker) {
+      healthLevel += 10;
+      highMedicalRiskFlag = true;
+    }
+    let certificationGrade = "regular";
+    if (scoringGuide.stateWithLowCertification(candidate.originState)) {
+      certificationGrade = "low";
+      result -= 5;
+    }
+    // このようなコードがずっと続く
+    result -= Math.max(healthLevel - 5, 0);
+    return result;
   }
-  let certificationGrade = "regular";
-  if (scoringGuide.stateWithLowCertification(candidate.originState)) {
-    certificationGrade = "low";
-    result -= 5;
-  }
-  // このようなコードがずっと続く
-  result -= Math.max(healthLevel - 5, 0);
-  return result;
 }
