f36e90e0eff673c848c2eaea27412bd24667bb9c
blue32a <blue32a.jp@gmail.com>
2023-10-18 14:19:03 +0000

    コード例ーコンストラクタ本体の引き上げ

---
+++ src/ch12/03-pull-up-constructor-body/example1.js
@@ -0,0 +1,18 @@
+class Party {}
+
+class Eployee extends Party {
+  constructor(name, id, monthlyCost) {
+    super();
+    this._id = id;
+    this._name = name;
+    this._monthlyCost = monthlyCost;
+  }
+}
+
+class Department extends Party {
+  constructor(name, staff) {
+    super();
+    this._name = name;
+    this._staff = staff;
+  }
+}
