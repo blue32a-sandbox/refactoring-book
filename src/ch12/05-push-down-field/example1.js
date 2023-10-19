e3a0482b6f15d4a02ca6a6976ee8f0b929689c2a
blue32a <blue32a.jp@gmail.com>
2023-10-19 09:11:11 +0000

    コンストラクタに`関数宣言の変更`を行うーサブクラスによるタイプコードの置き換え

--- src/ch12/05-push-down-field/example1.js
+++ src/ch12/05-push-down-field/example1.js
@@ -1,5 +1,5 @@
 class Employee {
-  constructor(name, type) {
+  constructor(name) {
     this._name = name;
   }
   toString() {return `${this._name} (${this.type})`;}
@@ -19,9 +19,9 @@ class Manager extends Employee {
 
 function createEmployee(name, type) {
   switch (type) {
-    case "engineer": return new Engineer(name, type);
-    case "salesman": return new Salesman(name, type);
-    case "manager":  return new Manager(name, type);
+    case "engineer": return new Engineer(name);
+    case "salesman": return new Salesman(name);
+    case "manager":  return new Manager(name);
     default: throw new Error(`Employee cannot be of type ${type}`);
   }
 }
