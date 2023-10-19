43e6b13fa4d9c156e716e86be8e76d4c1e059a7f
blue32a <blue32a.jp@gmail.com>
2023-10-19 08:58:38 +0000

    `変数のカプセル化`を行い自己カプセルかするーサブクラスによるタイプコードの置き換え

--- src/ch12/05-push-down-field/example1.js
+++ src/ch12/05-push-down-field/example1.js
@@ -4,6 +4,7 @@ class Employee {
     this._name = name;
     this._type = type;
   }
+  get type() {return this._type;}
   validateType(arg) {
     if (!["engineer", "manager", "salesman"].includes(arg))
       throw new Error(`従業員のタイプコードが不正：${arg}`);
