0f131bb2753ff36a145e7f78d9334e629ab01946
blue32a <blue32a.jp@gmail.com>
2023-10-19 09:16:10 +0000

    間接的な継承の使用の例ーサブクラスによるタイプコードの置き換え

---
+++ src/ch12/05-push-down-field/example2.js
@@ -0,0 +1,18 @@
+class Employee {
+  constructor(name, type) {
+    this.validateType(type);
+    this._name = name;
+    this._type = type;
+  }
+  validateType(arg) {
+    if (!["engineer", "manager", "salesman"].includes(arg))
+      throw new Error(`従業員のタイプコードが不正：${arg}`);
+  }
+  get type()    {return this._type;}
+  set type(arg) {this._type = arg;}
+
+  get capitalizedType() {
+    return this._type.charAt(0).toUpperCase() + this._type.substr(1).toLowerCase();
+  }
+  toString() {return `${this._name} (${this.capitalizedType})`;}
+}
