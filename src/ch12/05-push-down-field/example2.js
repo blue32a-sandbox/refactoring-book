bd5b21fb3a4d4de0c2ea3440d55674587a6718d2
blue32a <blue32a.jp@gmail.com>
2023-10-19 09:23:22 +0000

    タイプフィールドにサブクラスによるタイプコードの置き換え`を行うーサブクラスによるタイプコードの置き換え

--- src/ch12/05-push-down-field/example2.js
+++ src/ch12/05-push-down-field/example2.js
@@ -10,17 +10,31 @@ class Employee {
   }
   get typeString() {return this._type.toString();}
   get type()    {return this._type;}
-  set type(arg) {this._type = new EmployeeType(arg);}
+  set type(arg) {this._type = Employee.createEmployeeType(arg);}
 
   get capitalizedType() {
     return this.typeString.charAt(0).toUpperCase() + this.typeString.substr(1).toLowerCase();
   }
   toString() {return `${this._name} (${this.capitalizedType})`;}
+
+  static createEmployeeType(aString) {
+    switch (aString) {
+      case "engineer": return new Engineer();
+      case "manager":  return new Manager();
+      case "salesman": return new Salesman();
+      default: throw new Error(`従業員のタイプコードが不正：${aString}`);
+    }
+  }
 }
 
 class EmployeeType {
-  constructor(aString) {
-    this._value = aString;
-  }
-  toString() {return this._value};
+}
+class Engineer extends EmployeeType {
+  toString() {return "engineer"};
+}
+class Manager extends EmployeeType {
+  toString() {return "manager"};
+}
+class Salesman extends EmployeeType {
+  toString() {return "salesman"};
 }
