f798ac6d4045661347dad6d63b6294dc5fcedd5e
blue32a <blue32a.jp@gmail.com>
2023-10-20 12:31:12 +0000

    createPersonに`変数のインライン化`を行うーサブクラスの削除

--- src/ch12/06-remove-subclass/person.js
+++ src/ch12/06-remove-subclass/person.js
@@ -17,13 +17,11 @@ class Female extends Person {
 }
 
 function createPerson(aRecord) {
-  let p;
   switch (aRecord.gender) {
-    case 'M': p = new Male(aRecord.name); break;
-    case 'F': p = new Female(aRecord.name); break;
-    default: p = new Person(aRecord.name);
+    case 'M': return new Male(aRecord.name);
+    case 'F': return new Female(aRecord.name);
+    default:  return new Person(aRecord.name);
   }
-  return p;
 }
 
 function loadFromInput(data) {
