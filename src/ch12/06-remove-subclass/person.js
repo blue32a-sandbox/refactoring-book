eed0b85650389ffb02f53374ea98f743e83b4f4a
blue32a <blue32a.jp@gmail.com>
2023-10-20 12:30:18 +0000

    インスタンス生成をするロジックに`関数の抽出`を行うーサブクラスの削除

--- src/ch12/06-remove-subclass/person.js
+++ src/ch12/06-remove-subclass/person.js
@@ -1,11 +1,11 @@
 class Person {
-    constructor(name) {
-      this._name = name;
-    }
+  constructor(name) {
+    this._name = name;
+  }
 
-    get name() {return this._name;}
-    get genderCode() {return "X";}
-    // 後は省略
+  get name() {return this._name;}
+  get genderCode() {return "X";}
+  // 後は省略
 }
 
 class Male extends Person {
@@ -16,16 +16,20 @@ class Female extends Person {
   get genderCode() {return "F";}
 }
 
+function createPerson(aRecord) {
+  let p;
+  switch (aRecord.gender) {
+    case 'M': p = new Male(aRecord.name); break;
+    case 'F': p = new Female(aRecord.name); break;
+    default: p = new Person(aRecord.name);
+  }
+  return p;
+}
+
 function loadFromInput(data) {
   const result = [];
   data.forEach(aRecord => {
-    let p;
-    switch (aRecord.gender) {
-      case 'M': p = new Male(aRecord.name); break;
-      case 'F': p = new Female(aRecord.name); break;
-      default: p = new Person(aRecord.name);
-    }
-    result.push(p);
+    result.push(createPerson(aRecord));
   });
   return result;
 }
