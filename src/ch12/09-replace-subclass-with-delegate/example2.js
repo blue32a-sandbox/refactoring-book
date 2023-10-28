4d5ef62ae775ff891769a191c188243ba17886a8
blue32a <blue32a.jp@gmail.com>
2023-10-28 13:07:16 +0000

    スーパークラスから委譲先のメソッドを呼び出すー委譲によるサブクラスの置き換え

--- src/ch12/09-replace-subclass-with-delegate/example2.js
+++ src/ch12/09-replace-subclass-with-delegate/example2.js
@@ -28,7 +28,9 @@ class Bird {
   get plumage() {
     return this._plumage || "average";
   }
-  get airSpeedVelocity() {return null;}
+  get airSpeedVelocity() {
+    return this._speciesDelegate ? this._speciesDelegate.airSpeedVelocity : null;
+  }
 }
 
 class EuropeanSwallow extends Bird {
