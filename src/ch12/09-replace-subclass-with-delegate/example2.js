559ef8da7fb2b50702a90fe34d063640bbeae36f
blue32a <blue32a.jp@gmail.com>
2023-10-28 13:00:49 +0000

    階層の置き換えの例ー委譲によるサブクラスの置き換え

---
+++ src/ch12/09-replace-subclass-with-delegate/example2.js
@@ -0,0 +1,53 @@
+function createBird(bird) {
+  switch (bird.type) {
+  case 'EuropeanSwallow':
+    return new EuropeanSwallow(bird);
+  case 'AfricanSwallow':
+    return new AfricanSwallow(bird);
+  case 'NorwegianBlueParrot':
+    return new NorwegianBlueParrot(bird);
+  default:
+    return new Bird(bird);
+  }
+}
+
+class Bird {
+  constructor(data) {
+    this._name = data.name;
+    this._plumage = data.name;
+  }
+  get name() {return this._name;}
+  get plumage() {
+    return this._plumage || "average";
+  }
+  get airSpeedVelocity() {return null;}
+}
+
+class EuropeanSwallow extends Bird {
+  get airSpeedVelocity() {return 35;}
+}
+
+class AfricanSwallow extends Bird {
+  constructor(data) {
+    super(data);
+    this._numberOfCoconuts = data.numberOfCoconuts;
+  }
+  get airSpeedVelocity() {
+    return 40 - 2 * this.numberOfCoconuts;
+  }
+}
+
+class NorwegianBlueParrot extends Bird {
+  constructor(data) {
+    super(data);
+    this._voltage = data.voltage;
+    this._isNailed = data.isNailed;
+  }
+  get plumage() {
+    if (this.voltage > 100) return "scorched";
+    else return this._plumage || "beautiful";
+  }
+  get airSpeedVelocity() {
+    return (this.isNailed) ? 0 : 10 + this.voltage / 10;
+  }
+}
