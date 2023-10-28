c22e1ddc2e409be15e317328c26400b174288ee5
blue32a <blue32a.jp@gmail.com>
2023-10-28 12:50:31 +0000

    サブクラスを削除するー委譲によるサブクラスの置き換え

--- src/ch12/09-replace-subclass-with-delegate/example1.js
+++ src/ch12/09-replace-subclass-with-delegate/example1.js
@@ -25,13 +25,6 @@ class Booking {
   }
 }
 
-class PremiumBooking extends Booking {
-  constructor(show, date, extras) {
-    super(show, date);
-    this._extras = extras;
-  }
-}
-
 class PremiumBookingDelegate {
   constructor(hostBooking, extras) {
     this._host = hostBooking;
@@ -57,7 +50,7 @@ function createBooking(show, date) {
 }
 
 function createPremiumBooking(show, date, extras) {
-  const result = new PremiumBooking(show, date, extras);
+  const result = new Booking(show, date, extras);
   result._bePremium(extras);
   return result;
 }
