a0bab76bdfb007a4905426cd07309c2edb3879aa
blue32a <blue32a.jp@gmail.com>
2023-10-30 00:41:41 +0000

    Scrollオブジェクト群の読み込みー委譲によるスーパークラスの置き換え

--- src/ch12/10-replace-superclass-with-delegate/scroll.js
+++ src/ch12/10-replace-superclass-with-delegate/scroll.js
@@ -29,3 +29,10 @@ class Scroll {
     return this._lastCleaned.until(targetDate, ChronoUnit.DAYS);
   }
 }
+
+// load routine...
+const scrolls = aDocument
+  .map(record => new Scroll(record.id,
+                            record.catalogData.title,
+                            record.catalogData.tags,
+                            LocalDate.parse(record.lastCleaned)));
